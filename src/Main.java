3fc1bf629cff630df23d9ce05805ef30f640154e
dakshchaudhary16 <dakshdagur953@gmail.com>
2026-03-11 09:04:59 +0530

    Updated project to BookMyStayApp Use Case 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-public class Main {
+
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("     Hotel Booking System v1.0      ");
+        System.out.println("====================================");
+
+        System.out.println("Application started successfully.");
+
+        System.out.println("Thank you for using Book My Stay App.");
+    }
 }
---
+++ src/UseCase1BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class UseCase1BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("        Welcome to BookMyStayApp   ");
+        System.out.println("        Version 1.0                ");
+        System.out.println("===================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using BookMyStayApp.");
+    }
+}
