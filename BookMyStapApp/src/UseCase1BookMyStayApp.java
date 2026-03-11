ccad9f987b629cf7a2886afef0c3030764c48959
mvrhemanth2006-droid <mvrhemanth2006@gmail.com>
2026-03-11 09:41:21 +0530

    UseCase1

---
+++ BookMyStapApp/src/UseCase1BookMyStayApp.java
@@ -0,0 +1,12 @@
+public class UseCase1BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("        Welcome to BookMyStayApp   ");
+        System.out.println("        Version 1.0                ");
+        System.out.println("===================================");
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using BookMyStayApp.");
+    }
+}
